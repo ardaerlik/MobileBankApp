090cb9bbaafae1456b72cfd1a67f634f09e9af14
Arda Erlik <aerlik20@ku.edu.tr>
2021-12-01 21:51:44 +0300

    create project template

---
+++ MobileBankApp/MobileBankApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MobileBankApp
+//
+//  Created by Arda Erlik on 12/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
