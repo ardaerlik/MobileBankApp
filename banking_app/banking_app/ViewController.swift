8c9b8d0b4d39c97657c8a0d2a38c9eee936aa334
Can Koz <canxkoz@gmail.com>
2021-12-06 20:09:47 +0300

    Contains skeleton files of the view controllers
    simple non functional view controllers that are connected to one another through buttons. I have not used auto layout or added the tables yet.

---
+++ banking_app/banking_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  banking_app
+//
+//  Created by Can Koz on 6.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
