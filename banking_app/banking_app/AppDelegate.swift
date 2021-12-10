5c9662a128588704d2c5e31faa753309297a7d99
Arda Erlik <aerlik20@ku.edu.tr>
2021-12-10 22:37:20 +0300

    change AppDelegate

--- banking_app/banking_app/AppDelegate.swift
+++ banking_app/banking_app/AppDelegate.swift
@@ -6,8 +6,9 @@
 //
 
 import UIKit
-import Firebase
+
 import FirebaseCore
+import FirebaseFirestore
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
 
+        let db = Firestore.firestore()
+        
         return true
     }
     
