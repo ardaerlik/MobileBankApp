b47ce3ab4b9d202d7d99da6c3d0d695251a0e3fb
Arda Erlik <aerlik20@ku.edu.tr>
2021-12-10 21:39:14 +0300

    add pod frameworks and change AppDelegate

---
+++ banking_app/Pods/Target Support Files/Pods-banking_app/Pods-banking_app-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_banking_app : NSObject
+@end
+@implementation PodsDummy_Pods_banking_app
+@end
--- banking_app/banking_app/AppDelegate.swift
+++ banking_app/banking_app/AppDelegate.swift
@@ -6,17 +6,20 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseCore
 
-@main
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        FirebaseApp.configure()
+
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
