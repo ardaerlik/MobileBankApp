3eafee8362fc2f49db8d44e9760d23396d5445be
Arda Erlik <aerlik20@ku.edu.tr>
2021-12-22 17:07:58 +0300

    configure AccountsTableViewCell

---
+++ banking_app/banking_app/Cells/AccountsCollectionViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  AccountsCollectionViewCell.swift
+//  banking_app
+//
+//  Created by Arda Erlik on 12/22/21.
+//
+
+import UIKit
+
+class AccountsCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet private weak var accountTypeImageView: UIImageView!
+    @IBOutlet private weak var usableAmountLabel: UILabel!
+    @IBOutlet private weak var accountNumberLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.cornerRadius = 20
+    }
+    
+    func configure(with model: AccountModel?) {
+        // TODO: accountTypeImageView configuration
+        usableAmountLabel.text = "Usable Amount: \(model?.usableAmount ?? 0) \(model?.accountType ?? .TRY)"
+        accountNumberLabel.text = model?.accountNumber
+    }
+}
