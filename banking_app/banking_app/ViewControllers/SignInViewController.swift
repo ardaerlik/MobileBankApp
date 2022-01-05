80f56e95116f07ec80a2a19712ef981b66ad2ad0
Arda Erlik <aerlik20@ku.edu.tr>
2022-01-05 23:21:51 +0300

    keyboard configuration(1) / SignInVC

--- banking_app/banking_app/ViewControllers/SignInViewController.swift
+++ banking_app/banking_app/ViewControllers/SignInViewController.swift
@@ -20,6 +20,10 @@ class SignInViewController: UIViewController {
         errorLabel.textColor = .red
         tckn.delegate = self
         password.delegate = self
+        tckn.tag = 1
+        password.tag = 2
+        setupToolbarForTextField()
+        initializeHideKeyboard()
     }
     
     override func viewDidLoad() {
@@ -40,6 +44,43 @@ class SignInViewController: UIViewController {
             }
         }
     }
+    
+    private func setupToolbarForTextField() {
+        let barForPassword = UIToolbar()
+        let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(dismissKeyboard))
+        let previousButton = UIBarButtonItem(title: "", style: .plain, target: self, action: #selector(previousTextField))
+        previousButton.image = UIImage(systemName: "chevron.up")
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        barForPassword.items = [previousButton, flexSpace, doneButton]
+        barForPassword.sizeToFit()
+        password.inputAccessoryView = barForPassword
+        
+        let barForTckn = UIToolbar()
+        let nextButton = UIBarButtonItem(title: "", style: .plain, target: self, action: #selector(nextTextField))
+        nextButton.image = UIImage(systemName: "chevron.down")
+        barForTckn.items = [nextButton, flexSpace, flexSpace]
+        barForTckn.sizeToFit()
+        tckn.inputAccessoryView = barForTckn
+    }
+    
+    private func initializeHideKeyboard() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        self.view.addGestureRecognizer(tap)
+    }
+    
+    @objc private func dismissKeyboard() {
+        self.view.endEditing(true)
+    }
+    
+    @objc private func previousTextField() {
+        //password.resignFirstResponder()
+        tckn.becomeFirstResponder()
+    }
+    
+    @objc private func nextTextField() {
+        //tckn.resignFirstResponder()
+        password.becomeFirstResponder()
+    }
 }
 
 extension SignInViewController: UITextFieldDelegate {
@@ -48,11 +89,11 @@ extension SignInViewController: UITextFieldDelegate {
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        if textField == tckn || textField == password {
-            textField.resignFirstResponder()
-            return false
+        if let nextField = self.view.viewWithTag(textField.tag + 1) as? UITextField {
+            nextField.becomeFirstResponder()
         } else {
-            return true
+            textField.resignFirstResponder()
         }
+        return false
     }
 }
