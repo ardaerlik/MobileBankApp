c2cf32fb459e281319dbb04ec4ae7b98294d6ff4
Arda Erlik <aerlik20@ku.edu.tr>
2022-01-09 14:49:20 +0300

    change Settings View Controller

--- banking_app/banking_app/ViewControllers/SettingsViewController.swift
+++ banking_app/banking_app/ViewControllers/SettingsViewController.swift
@@ -13,6 +13,8 @@ class SettingsViewController: UIViewController {
     @IBOutlet private weak var userNameLabel: UILabel!
     @IBOutlet private weak var tcknLabel: UILabel!
     @IBOutlet private weak var gsmLabel: UILabel!
+    @IBOutlet private weak var emailLabel: UILabel!
+    @IBOutlet private weak var occupationLabel: UILabel!
     @IBOutlet private weak var addressTextView: UITextView!
     
     override func viewDidLoad() {
@@ -27,13 +29,36 @@ class SettingsViewController: UIViewController {
     
     private func setUI(with model: UserModel) {
         self.title = "Settings"
+        avatarImageView.load(imageLocation: model.avatar)
+        avatarImageView.layer.masksToBounds = true
+        avatarImageView.layer.borderWidth = 3
+        avatarImageView.layer.borderColor = UIColor.link.cgColor
+        avatarImageView.layer.cornerRadius = avatarImageView.bounds.width / 2
         userNameLabel.text = model.username
         tcknLabel.text = model.tckn
         gsmLabel.text = model.gsm
+        emailLabel.text = model.email
+        occupationLabel.text = "\(model.occupation) - \(model.company)"
         addressTextView.text = model.address
+        addressTextView.backgroundColor = .darkGray
     }
     
     @IBAction func signOutTapped(_ sender: Any) {
         navigationController?.popToRootViewController(animated: true)
+    }
+}
+
+extension UIImageView {
+    func load(imageLocation: String) {
+        DispatchQueue.global().async { [weak self] in
+            let url = URL(string: imageLocation)
+            if let data = try? Data(contentsOf: url!) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.image = image
+                    }
+                }
+            }
+        }
     }
 }
